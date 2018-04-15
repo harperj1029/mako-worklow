bf8596e5b5c7dfc5f3f5cf9ad6a352fc09114585
Jason Harper <harperj1029@gmail.com>
2018-04-15 16:13:02 -0400

    extra import to avoid code split points with codesandbox.io

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.scss';
 import {BrowserRouter} from "react-router-dom";
+//NOTE: This import is only here to satisfy codesandbox.io as it was having trouble with lazy-loaded (code split point) modules.
+import from "./components/workflow-instances/workflow1/index";
 
 ReactDOM.render((
         <BrowserRouter>
