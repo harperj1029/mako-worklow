b79f9029f81eca100262c23f13977dda65742885
Jason Harper <harperj1029@gmail.com>
2018-04-15 16:22:37 -0400

    tweak

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.scss';
 import {BrowserRouter} from "react-router-dom";
-//NOTE: This import is only here to satisfy codesandbox.io as it was having trouble with lazy-loaded (code split point) modules.
+//NOTE: This import is only here to satisfy codesandbox.io as it was having trouble with lazy-loaded
+// (code split point) modules.
 import "./components/workflow-instances/workflow1/index";
 
 ReactDOM.render((
